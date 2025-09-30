a232b4e94c8cd2d247763c3e4b5a372045c431eb
Adrian <adriankubinyete@outlook.com>
2025-09-30 14:40:18 -0300

    fix: motd swaps to profile.d, remove issabel login-info.sh

--- motdswapper.sh
+++ motdswapper.sh
@@ -8,8 +8,18 @@ if [[ -f /usr/local/bin/motd.sh ]]; then
     cp /usr/local/bin/motd.sh motdswapper-bkp/
 fi
 
+if [[ -f /etc/profile.d/motd.sh ]]; then
+    echo "Salvando backup de /etc/profile.d/motd.sh em motdswapper-bkp/"
+    cp /etc/profile.d/motd.sh motdswapper-bkp/
+fi
+
+if [[ -f /etc/profile.d/login-info.sh ]]; then
+    echo "Salvando backup de /etc/profile.d/login-info.sh em motdswapper-bkp/"
+    cp /etc/profile.d/login-info.sh motdswapper-bkp/
+fi
+
 echo "Instalando motd.sh em /usr/local/bin/motd.sh"
-cp motd.sh /usr/local/bin/motd.sh
+cp motd.sh /etc/profile.d/motd.sh
 chmod +x /usr/local/bin/motd.sh
 
 if [[ -f /etc/motd ]]; then
